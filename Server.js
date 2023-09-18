02584936e130e8e9e604a30077b8e73cefb96426
Kebinsan <prokevint@gmail.com>
2023-09-18 17:55:50 -0400

    second commit

--- Server.js
+++ Server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
