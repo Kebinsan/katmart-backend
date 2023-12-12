d5c2a3f74f78ee2b646733a942f3e9810e2b3ce4
Kebinsan <prokevint@gmail.com>
2023-12-11 20:12:59 -0500

    rolledback

--- server.js
+++ server.js
@@ -21,23 +21,18 @@ app.use(cors());
 
 //generates a JWT token with username and token secret
 function generateAccessToken(username) {
-  return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: "1800s" });
+  return jwt.sign(username, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1800s" });
 }
 
 //authenticates the token when post/get requires it
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-
   if (token == null) return res.sendStatus(401);
 
-  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-    console.log(err);
-
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
-
     req.user = user;
-
     next();
   });
 }
